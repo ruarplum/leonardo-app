b57ca81956f5484fd8909a848b43ddf6a564450a
Nicholas Ferrari <Nick.ferrari@me.com>
2021-02-22 15:28:41 +0000

    #8 creating test text

--- src/Components/News-Feed/News-Feed.js
+++ src/Components/News-Feed/News-Feed.js
@@ -5,16 +5,22 @@ const Feed = (props) => {
     return (
         <div className = "feed-box">
             <div className = "feed-sidebar">
+                <h2>test texts</h2>
             </div>
             <div className = "feed-com-post">
+                <h3>r/AskReddit <span className = "feed-span">Posted by u/nick 12 hours ago</span></h3>
             </div>
             <div className = "feed-award">
+                <p>test text</p>
             </div>
             <div className = "feed-join">
+                <button>test</button>
             </div>
             <div className = "feed-main">
+                <p>This is test text</p>
             </div>
             <div className = "feed-bottom-bar">
+                <button>bottom test</button>
             </div>
         </div>
     );
