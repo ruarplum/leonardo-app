970f12735d731c368688da7f44039fd00bf799e6
Ruaraidh <rca.plummer@gmail.com>
2021-02-22 18:42:35 +0000

    #4.4 - end of play - Monday

--- src/Components/Popular-posts-navbar/Popular-posts-navbar.js
+++ src/Components/Popular-posts-navbar/Popular-posts-navbar.js
@@ -1,22 +1,25 @@
-import React from 'react'
 import './Popular-posts-navbar.css'
 
-
 const PopularNavbar = () => {
-    return (
-        <navbar>
-            <div>
-            <h2>Hot</h2>
-            </div>
-            <div>
-            <h2>United Kingdom</h2>
-            </div>
-            <div>
-            <h3>New</h3>
-            </div>
-            <h3>Top</h3>
 
-        </navbar>
+    return (
+        <nav className="PopNavbar">
+            <div>
+                <button className="buttonBlue">Testing Hot</button>
+            </div>
+            <div>
+                <button className="buttonBlue">Test Select your location dropdown - United Kingdom</button>
+            </div>
+            <div>
+                <button className="buttonGrey">Test New-Post</button>
+            </div>
+            <div>
+                <button className="buttonGrey">Test Top-Posts</button>
+            </div>
+            <div>
+                <button>TEST ... Expand menu</button>
+            </div>
+        </nav>
     )
 }
 
