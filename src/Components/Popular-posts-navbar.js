4150fc2befaf1902138133cc4d9c32196410eb77
Ruaraidh <rca.plummer@gmail.com>
2021-02-22 15:28:09 +0000

    #4.1 - Adding directory and basic files

---
+++ src/Components/Popular-posts-navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Popular-posts-navbar.css'
+
+
+const PopularNavbar = () => {
+    return (
+        <navbar>
+            <h2>Hot</h2>
+            <h2>United Kingdom</h2>
+            <h3>New</h3>
+            <h3>Top</h3>
+            
+        </navbar>
+    )
+}
+
+export default Popular-posts-navbar
