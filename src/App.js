602b70a15de2c0be22e75d08b1bb1d6b520cc14c
Gemcila <ggemcila@gmail.com>
2021-02-22 19:44:49 -0800

    #7 Changed from "functional component" into "class based component"

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from "react";
 import Sidebar from "./components/Sidebar";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header>Communities Around the World</header>
-    <Sidebar text="r/Humboldt" />
-    <Sidebar text="r/boulder" />
-    <Sidebar text="r/GrandForks" />
-    <Sidebar text="r/corona" />
-    <Sidebar text="r/Westchester" />
 
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>Communities Around the World</header>
+        <Sidebar text="r/Humboldt" />
+        <Sidebar text="r/boulder" />
+        <Sidebar text="r/GrandForks" />
+        <Sidebar text="r/corona" />
+        <Sidebar text="r/Westchester" />
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -4,7 +4,7 @@ class Sidebar extends React.Component {
     render() {
         return (
             <div>
-                <img src={this.props.image} alt={this.props.text}/>
+                 
                 <p>{this.props.text}</p>
             </div>
         )
