d117049168badf6880678def7076fd2b72e77558
Gemcila <ggemcila@gmail.com>
2021-02-22 19:53:18 -0800

    #7 Passed "logo" image through props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Sidebar from "./components/sidebar/Sidebar";
 import './App.css';
-
+import logo from "./components/sidebar/logo.png";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header>Communities Around the World</header>
-        <Sidebar text="r/Humboldt" />
-        <Sidebar text="r/boulder" />
-        <Sidebar text="r/GrandForks" />
-        <Sidebar text="r/corona" />
-        <Sidebar text="r/Westchester" />
+        <Sidebar text="r/Humboldt" image={logo}/>
+        <Sidebar text="r/boulder" image={logo} />
+        <Sidebar text="r/GrandForks" image={logo} />
+        <Sidebar text="r/corona" image={logo} />
+        <Sidebar text="r/Westchester" image={logo}/>
 
       </div>
     );
