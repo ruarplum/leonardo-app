ac1e01c93a836c226c3b8fc2b2ccd54cdcdd1d19
Muge <mafromist@gmail.com>
2021-02-23 07:02:30 +0000

    #5 Trending Today Container and News has added
    Images and contents have added into the folder, styling has started

---
+++ src/Components/TrendingToday#5/News.js
@@ -0,0 +1,21 @@
+import "./News.css";
+
+const News = (props) => {
+  return (
+    <div
+      className="container"
+      style={{ backgroundImage: `url(${props.newsImg})` }}
+    >
+      <div>
+        <h3 className="newsTitle">{props.newsTitle}</h3>
+        <h4 className="newsSubtitle">{props.newsSubtitle}</h4>
+      </div>
+      <div className="commArea">
+          <img src={props.commPic} alt={props.commName} className="commPic"/>
+          <p className="commName">{props.commName}</p>
+      </div>
+    </div>
+  );
+};
+
+export default News;
