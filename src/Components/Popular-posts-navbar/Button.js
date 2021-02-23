16e9ac24b9adb091c2a782046dd7563ede10ca10
Ruaraidh <rca.plummer@gmail.com>
2021-02-23 14:43:41 +0000

    #4.6 - Create button component and import to pop-navbar

---
+++ src/Components/Popular-posts-navbar/Button.js
@@ -0,0 +1,16 @@
+import { FaGithub } from 'react-icons/fa';
+
+const Button = ({ color, text, onClick }) => {
+    return (
+        <button
+            style={{ backgroundColor: color }}
+            className="button"
+            onClick={onClick}
+            
+            >
+                {text}
+            </button>
+    )
+}
+
+export default Button
--- src/Components/Popular-posts-navbar/Popular-posts-navbar.js
+++ src/Components/Popular-posts-navbar/Popular-posts-navbar.js
@@ -1,24 +1,16 @@
 import './Popular-posts-navbar.css'
+import Button from './Button'
 
 const PopularNavbar = () => {
 
     return (
         <nav className="PopNavbar">
-            <div>
-                <button className="buttonBlue">Testing Hot</button>
-            </div>
-            <div>
-                <button className="buttonBlue">Test Select your location dropdown - United Kingdom</button>
-            </div>
-            <div>
-                <button className="buttonGrey">Test New-Post</button>
-            </div>
-            <div>
-                <button className="buttonGrey">Test Top-Posts</button>
-            </div>
-            <div>
-                <button>TEST ... Expand menu</button>
-            </div>
+            <Button text='Best' />
+            <Button text='Hot' />
+            <Button text='New' />
+            <Button text='Top' />
+            <Button text='...' />
+            <Button text='View' />  
         </nav>
     )
 }
