51258ee1d28bb59eb684084fc253f5e40e28deab
Ruaraidh <rca.plummer@gmail.com>
2021-02-25 11:12:39 +0000

    #4.1 - sort function

---
+++ src/Components/Popular-posts-navbar/Sort.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Sort = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Sort
+
+
+//? const SortFunc = NewsFeed.(VALUE TO BE SORTED).sort ((high, low) => high - low);
+//! Need to target the Counter for example which is in Post.js within the Post component
+//* HOT = trending/most clicks etc
+
