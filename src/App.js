15a584635d2961696d488c4e518d9b30d962142f
Nicholas Ferrari <Nick.ferrari@me.com>
2021-02-26 15:23:20 +0000

    neilwanted

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
       spoon: 19,
       hugz: 0,
       time: 8,
-      titleText: "Wanted for Crimes against CSS",
+      textTitle: "Wanted for Crimes against CSS",
       textBody: "This individual is wanted for questioning over his true feelings about CSS and what his motives may be!",
       img:<img id='newImage' src={NMunro}/>,
     },
