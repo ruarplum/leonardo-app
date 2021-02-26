a591d4b6c51bb778b513342d0a3bd439c4118acb
Gemcila <ggemcila@gmail.com>
2021-02-26 11:42:19 -0800

    #102 Codenation advertismenet sidebar completed

--- src/Components/Advertisement-sidebar/AdvertSidebar.js
+++ src/Components/Advertisement-sidebar/AdvertSidebar.js
@@ -1,23 +1,36 @@
 import React from "react";
 import advertlogo from "./assets/advertlogo.png";
+import codenationLogo from "./assets/codenationLogo.png";
 import "./AdvertSidebar.css";
 
 const AdvertSidebar = () => {
     return (
-        <div className="AdvertSidebar">
-           
-            <div className="advertTitle">
-                <p>ADVERTISEMENT</p>
-            </div>
-            <div id="advertcontainer"> 
-            <div className="advertcontainer">
-                <h2 className="skyTitle">   Super reliable, Superfast Broadband</h2>
-                <img id="advertlogo" src={advertlogo} alt="advertlogo"></img>
-                <h1 className="skySubtitle">sky</h1>
-                <h3 className="clickforlegals">Click for legals</h3>
-            </div>
+        <div>
+            <div className="Codenationadvert">
+                <div className="advertTitle">
+                    <p>ADVERTISEMENT</p>
+                </div>
+                <div className="advertcontainer1">
+                    <img id="codenationLogo" src={codenationLogo} alt="codenationLogo"></img>
+                <div className="CodenationLink"><a href="https://wearecodenation.com/" id="CodenationLink">Click to see more about CODENATION</a></div>
+                </div>
             </div>
 
+            <div className="AdvertSidebar">
+
+                <div className="advertTitle">
+                    <p>ADVERTISEMENT</p>
+                </div>
+                <div id="advertcontainer">
+                    <div className="advertcontainer2">
+                        <h2 className="skyTitle">   Super reliable, Superfast Broadband</h2>
+                        <img id="advertlogo" src={advertlogo} alt="advertlogo"></img>
+                        <h1 className="skySubtitle">sky</h1>
+                        <h3 className="clickforlegals">Click for legals</h3>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
